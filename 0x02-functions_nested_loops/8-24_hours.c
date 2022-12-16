670858ef1e2a4fd30b81d13f1bb3d699160affdb
maina-al3x <alexwachira4@gmail.com>
2022-12-15 18:46:13 -0800

    I'm federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,13 +13,17 @@ void jack_bauer(void)
 
 	while (i < 24)
 	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		_putchar(':');
-		_putchar((j / 10) + '0');
-		_putchar((j / 10) + '0');
-		_putchar('\n');
-		j++;
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
 	}
-	i++;
 }
