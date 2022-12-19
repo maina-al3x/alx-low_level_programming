092870168a5f8a5c83ff2ccfeaef8b9db9d2a557
maina-al3x <alexwachira4@gmail.com>
2022-12-19 14:13:25 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - prints single charatcer to standard output
+ * @c: Character to print
+ *
+ * Reutrn: on success 1
+ * on error, -1 is returned and errno is set accordingly
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
