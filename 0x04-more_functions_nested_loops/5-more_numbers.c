efa67d769b4a1549f3c8bab5b24f73f36112e907
maina-al3x <alexwachira4@gmail.com>
2022-12-21 00:48:20 -0800

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers
- * from 0 to 14
- * followed by a new line.
- *
+ * more_numbers - prints the numbers from 0 to 14 ten times each on a new line
  * Return: returns nothing
  */
 
@@ -22,6 +19,6 @@ void more_numbers(void)
 				_putchar((j % 10) + '0');
 			}
 		}
-		_putchar('\n');
+		_putchar(10);
 	}
 }
