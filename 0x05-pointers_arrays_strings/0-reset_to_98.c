cec20842292d5a6e396af87b65fc5132e07f4ee2
maina-al3x <alexwachira4@gmail.com>
2022-12-22 11:24:27 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as a parameter
  * and updates the value to 98
+ * @n: - new value to update to pointer
  */
 
 void reset_to_98(int *n)
