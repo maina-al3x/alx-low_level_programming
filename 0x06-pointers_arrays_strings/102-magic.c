f0ba221e4b1b551ffc778d911e4051b1d23d75cd
maina-al3x <alexwachira4@gmail.com>
2023-01-07 02:12:05 -0800

    Advtask10 10. A dream

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/**
- * main - a program that prints a[2] = 98
- * followed by a new line
- * Return: 0
- */
+
 int main(void)
 {
 	int n;
@@ -12,7 +8,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
+	/* main - magic
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
