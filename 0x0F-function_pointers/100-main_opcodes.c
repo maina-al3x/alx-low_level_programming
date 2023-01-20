3cd53f3c0230cb097f1d8bbe0329bd31fa34fd70
maina-al3x <alexwachira4@gmail.com>
2023-01-19 19:07:00 -0800

    Task4 4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
- * main - searchs for an integer
- * @argc: number of arguments
- * @argv: array with arguments
- *
- * Return: no return
+ * main- Entry point
+ * @argc: the number of parameters.
+ * @argv: the parameeters in the case the number ob bytes.
+ * Description: this program prints opcodes in hexa)?
+ * Return: 0 in succes
  */
 
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	int i, n;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-			exit(1);
+		return (1);
 	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	arr = (char *)main;
-	for (i = 0; i < bytes; ++i)
+	for (i = 0; i < n; i++)
 	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx\n", arr[i]);
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
