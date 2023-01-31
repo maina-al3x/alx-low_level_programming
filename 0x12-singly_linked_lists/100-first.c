ee1b7e6ddd962004565040140c83bc241b07b584
maina-al3x <alexwachira4@gmail.com>
2023-01-31 03:49:49 -0800

    Task 5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+
+void print_before_main(void) __attribute__ ((constructor));
+
+/**
+ * print_before_main - function that prints before the main
+ * function is executed
+ *
+ * Return: void
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
