62d8cf4e5ef7326615a3fac042e29e69c2b8d051
maina-al3x <alexwachira4@gmail.com>
2023-02-02 18:25:18 -0800

    Task 5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ *  @head: double pointer to the struct listint_t
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	if (*head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
