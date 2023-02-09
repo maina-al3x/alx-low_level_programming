1965868e22fb49ec9f017ab959404b6aa8049d9f
maina-al3x <alexwachira4@gmail.com>
2023-02-08 19:00:24 -0800

    Task 4. elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+int _putchar(char c);
 int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
